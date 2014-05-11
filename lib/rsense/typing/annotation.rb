0a91bb080432df4b11be9ce32790e5ddc9898f19
Eric West <eric.west@lonelyplanet.com>
2014-05-11 04:28:48 -0500

    Make Rsense classes easier to access from ruby

---
+++ lib/rsense/typing/annotation.rb
@@ -0,0 +1,20 @@
+module Rsense
+  module Typing
+    module Annotation
+      import Java::org.cx4a.rsense.typing.annotation::ClassType
+      import Java::org.cx4a.rsense.typing.annotation::MethodType
+      import Java::org.cx4a.rsense.typing.annotation::TypeAnnotation
+      import Java::org.cx4a.rsense.typing.annotation::TypeAny
+      import Java::org.cx4a.rsense.typing.annotation::TypeApplication
+      import Java::org.cx4a.rsense.typing.annotation::TypeConstraint
+      import Java::org.cx4a.rsense.typing.annotation::TypeExpression
+      import Java::org.cx4a.rsense.typing.annotation::TypeIdentity
+      import Java::org.cx4a.rsense.typing.annotation::TypeOptional
+      import Java::org.cx4a.rsense.typing.annotation::TypePragma
+      import Java::org.cx4a.rsense.typing.annotation::TypeSplat
+      import Java::org.cx4a.rsense.typing.annotation::TypeTuple
+      import Java::org.cx4a.rsense.typing.annotation::TypeUnion
+      import Java::org.cx4a.rsense.typing.annotation::TypeVariable
+    end
+  end
+end
