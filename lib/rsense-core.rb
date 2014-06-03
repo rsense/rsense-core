7de033d5e91f97492a4c8cc11f5bd973d7671539
Eric West <eric.west@lonelyplanet.com>
2014-06-03 15:42:36 -0500

    Adds a convenience method for getting to stubs

--- lib/rsense-core.rb
+++ lib/rsense-core.rb
@@ -3,6 +3,7 @@ require "pathname"
 require "jruby-parser"
 require "rsense/core/version"
 require "rsense.jar"
+require "filetree"
 
 jarpath = Pathname.new(__FILE__).dirname.join("jars").expand_path
 jarpath.children.each do |c|
@@ -15,6 +16,13 @@ require "rsense/ruby"
 require "rsense/parser"
 
 module Rsense
+
+  def self.root_dir
+    FileTree.new(File.dirname(__FILE__)).expand_path.ancestors[0]
+  end
+
+  BUILTIN = root_dir().join('stubs')
+
   import Java::org.cx4a.rsense::CodeAssist
   import Java::org.cx4a.rsense::CodeAssistError
   import Java::org.cx4a.rsense::CodeAssistResult
